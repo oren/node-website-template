6a60c5e40c3495a899a67d96e7423324ef9e51e4
Oren <ogolan@yp.com>
2013-05-26 01:38:23 -0700

    require the server return object with start() function

--- server.js
+++ server.js
@@ -11,34 +11,52 @@ var Templar = require('templar');
 
 // my module
 var router = require('./router.js');
+var server = null;
 
 var environment = process.env.NODE_ENV || 'development';
 var config = require('./config/' + environment + '.js');
 var templarOptions = { engine: config.engine, folder: config.templates };
 Templar.loadFolder(config.templates);
 
-// request goes here
-var server = http.createServer(function(req, res) {
-  
-  res.error = ErrorPage(req, res, {
-    404: 'not found!'
-  });
-  res.template = Templar(req, res, templarOptions);
-  router.match(req.url).fn(req, res, config);
-
-});
-
-// if this file is run directly (node server.js) 
+// if this file is run directly
+// create the server and listen immediatly 
+// 
+// usage: node server.js
+//
+// if it's being required, just create the server and return an object with start() function.
+// 
+// usage:
+//
+// server = require('./server')(config)
+// server.start(function(err) {
+//   console.log('server is ready')
+// }
 if (module === require.main) {
+  server = create();
   server.listen(config.port, function() {
     console.log('Server Listening - http://localhost:' + config.port + '. ' + environment + ' environment');
   });
-} else { // if it was required
-  module.exports.start = function(cb) {
-    server.listen(config.port, function() {
-      console.log('Server Listening - http://localhost:' + config.port + '. ' + environment + ' environment');
-      cb(null);
-    });
+} else {
+  module.exports = function(config) {
+    server = create();
+    return {
+      start: function(cb) {
+        server.listen(config.port, function() {
+          console.log('Server Listening - http://localhost:' + config.port + '. ' + environment + ' environment');
+          cb(null);
+        });
+      }
+    }
   }
 };
 
+function create() {
+  // request goes here
+  return http.createServer(function(req, res) {
+    res.error = ErrorPage(req, res, {
+      404: 'not found!'
+    });
+    res.template = Templar(req, res, templarOptions);
+    router.match(req.url).fn(req, res, config);
+  });
+};
--- test/server/index.js
+++ test/server/index.js
@@ -3,7 +3,7 @@
 var test = require('tape');
 var http = require('http');
 
-var config = require('./config/test.js');
+var config = require('../../config/test.js');
 var server = require('../../server.js')(config);
 
 test('/health returns 200', function (t) {
